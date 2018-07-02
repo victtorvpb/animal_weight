72ad4bd9aa31889b64075ec16b74e074b197c2de
Victor Pereira <victtor.vpb@gmail.com>
2018-07-01 22:15:21 -0300

    add model AnimalWeigthModel in admin

--- apps/animal_weigth/admin.py
+++ apps/animal_weigth/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import FarmModel
+from .models import FarmModel, AnimalWeigthModel
 
 # Register your models here.
 
 admin.site.register(FarmModel)
+admin.site.register(AnimalWeigthModel)
